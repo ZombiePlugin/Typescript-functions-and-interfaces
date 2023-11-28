29020deb1d88c83501c513cd6c5bf7a75304e95a
M <muzameelahmed1@gmail.com>
2023-11-28 16:50:03 +0000

    Interfaces

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Hello World!");
+function basicFunction(a, b) {
+    return a + b;
+}
+basicFunction(2, 3); // returns 5
+// optional parameters
+function optionalParamFunction(a, b, c) {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+}
+optionalParamFunction(1, 2); // returns 3
+optionalParamFunction(1, 2, 3); // returns 6
+// default parameters
+function defaultParamFunction(a, b, c) {
+    if (c === void 0) { c = 3; }
+    return a + b + c;
+}
+defaultParamFunction(1, 2, 3); // returns 6
+defaultParamFunction(1, 2); // returns 6
+// rest paramters
+function restParamsFunction(greeting) {
+    var names = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        names[_i - 1] = arguments[_i];
+    }
+    return "".concat(greeting, " ").concat(names.join(", "), "!");
+}
+restParamsFunction("Hello", "Steve", "Bill"); // returns "Hello Steve, Bill!"
+restParamsFunction("Hello"); // returns "Hello !"
+function add(a, b) {
+    return a + b;
+}
+add("Hello ", "Steve"); // returns "Hello Steve" 
+add(10, 20); // returns 30 
+function printUser(currentUser) {
+    console.log(currentUser.name);
+}
+var newUser = { name: "James", age: 23 };
+printUser(newUser);
+function getPhone(_a) {
+    var _b = _a.name, name = _b === void 0 ? "Iphone 15 Pro Max" : _b, _c = _a.colour, colour = _c === void 0 ? "blue" : _c, _d = _a.proModel, proModel = _d === void 0 ? true : _d;
+    console.log(name);
+    console.log(colour);
+    console.log(proModel);
+}
+getPhone({ name: "Samsung Galaxy" });
+getPhone({ name: "Samsung Galaxy", colour: "black" });
+getPhone({ name: "Samsung Galaxy", colour: "black", proModel: false });
+function changePassword(user, newPass) {
+    user.password = newPass;
+    console.log(user.password);
+}
+changePassword({ username: "billy123", password: 'pass123' }, "newPass321");
--- index.ts
+++ index.ts
@@ -1,1 +1,94 @@
-console.log("Hello World!");
+function basicFunction(a, b) {
+  return a + b;
+}
+
+basicFunction(2, 3); // returns 5
+
+
+// optional parameters
+function optionalParamFunction(a: number, b: number, c?: number) {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+}
+
+optionalParamFunction(1, 2); // returns 3
+optionalParamFunction(1, 2, 3); // returns 6
+
+
+// default parameters
+function defaultParamFunction(a: number, b: number, c = 3) {
+  return a + b + c;
+}
+
+defaultParamFunction(1, 2, 3); // returns 6
+defaultParamFunction(1, 2); // returns 6
+
+
+// rest paramters
+function restParamsFunction(greeting:string, ...names:string[]): string {
+    return `${greeting} ${names.join(", ")}!`
+}
+
+restParamsFunction("Hello", "Steve", "Bill"); // returns "Hello Steve, Bill!"
+restParamsFunction("Hello");// returns "Hello !"
+
+
+// function overloading
+function add(a:string, b:string):string;
+
+function add(a:number, b:number): number;
+
+function add(a: any, b:any): any {
+    return a + b;
+}
+
+add("Hello ", "Steve"); // returns "Hello Steve" 
+add(10, 20); // returns 30 
+
+
+// interfaces
+interface User {
+    name: String,
+    age: Number
+}
+
+function printUser(currentUser : User) {
+    console.log(currentUser.name);
+}
+
+let newUser = {name: "James", age: 23};
+printUser(newUser);
+
+
+// read-only interface
+interface PhoneConfig {
+    name? : string,
+    colour?: string,
+    proModel?: boolean
+}
+
+function getPhone({ name="Iphone 15 Pro Max", colour="blue", proModel=true} : PhoneConfig) {
+    console.log(name);
+    console.log(colour);
+    console.log(proModel);
+}
+
+getPhone({name:"Samsung Galaxy"});
+getPhone({name:"Samsung Galaxy", colour:"black"});
+getPhone({name:"Samsung Galaxy", colour:"black", proModel:false});
+
+
+// read-only interface
+interface userData {
+    readonly username : string,
+    readonly password : string
+}
+
+function changePassword(user : userData, newPass : string) {
+    user.password = newPass; // allows password change, however throws an error during development.
+    console.log(user.password);
+}
+
+changePassword({username: "billy123", password: 'pass123'}, "newPass321");
